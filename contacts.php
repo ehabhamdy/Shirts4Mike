ce1c680143b1b8bcda8a826fbb3d4192a4ebb936
ehabhamdy <ehabhamdy@gmail.com>
2014-12-05 23:33:19 +0200

    Contact form processing #2

--- contacts.php
+++ contacts.php
@@ -1,4 +1,19 @@
 <?php
+
+  if($_SERVER["REQUEST_METHOD"] == "POST")
+  {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $msg = $_POST["msg"];
+
+    //TODO: Send Email
+
+    header("Location: contacts.php?status=thanks");
+    exit;
+  }
+?>
+
+<?php
   $page_name = "Contacts";
   include("inc/header.php");
 ?>
@@ -6,36 +21,41 @@
 <div class="page section">
   <div class="wrapper">
     <h1>Contacts</h1>
-    <form  action="process-contacts.php" method="post">
-      <table>
-        <tr>
-          <th>
-            <label for="name">Name</label>
-          </th>
-          <td>
-            <input type="text" name="name" placeholder="Name">
-          </td>
-        </tr>
-        <tr>
-          <th>
-            <label for="email">Email</label>
-          </th>
-          <td>
-            <input type="text" name="email" placeholder="Email">
-          </td>
-        </tr>
-        <tr>
-          <th>
-            <label for="msg">Message</label>
-          </th>
-          <td>
-            <textarea name="msg" rows="8" cols="40"></textarea>
-          </td>
-        </tr>
-      </table>
-      <input type="submit" name="submit" value="Submit">
-    </form>
-
+    <?php if(isset($_GET["status"]) AND $_GET["status"] == "thanks") { ?>
+      <p>
+        Thank you, we will contact you very soon
+      </p>
+    <?php } else{?>
+      <form  action="contacts.php" method="post">
+        <table>
+          <tr>
+            <th>
+              <label for="name">Name</label>
+            </th>
+            <td>
+              <input type="text" name="name" placeholder="Name">
+            </td>
+          </tr>
+          <tr>
+            <th>
+              <label for="email">Email</label>
+            </th>
+            <td>
+              <input type="text" name="email" placeholder="Email">
+            </td>
+          </tr>
+          <tr>
+            <th>
+              <label for="msg">Message</label>
+            </th>
+            <td>
+              <textarea name="msg" rows="8" cols="40"></textarea>
+            </td>
+          </tr>
+        </table>
+        <input type="submit" name="submit" value="Submit">
+      </form>
+    <?php } ?>
   </div>
 
 
